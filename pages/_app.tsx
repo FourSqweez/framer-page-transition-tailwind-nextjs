25789cdb53824d775fa899ed6f6ec281a3686b56
Four <jeerasak.kr@chomchob.com>
2022-11-24 18:43:41 +0700

    add : animatePresence

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '../src/components/Navbar'
+import { AnimatePresence } from 'framer-motion'
+import { useRouter } from 'next/router'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps, router }: AppProps) {
   return (
-    <div className='py-24 px-12 lg:px-48 font-latoBold'>
+    <div className='py-12 px-12 lg:px-48 font-latoBold'>
       <Navbar />
-      <Component {...pageProps} />
+      <AnimatePresence initial={false }>
+        <Component key={router.pathname} {...pageProps} />
+      </AnimatePresence>
     </div>
   )
 }
+
+ 
