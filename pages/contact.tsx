2a12e352ede12da4cbcb7fef2cf6fd65010f782a
Four <jeerasak.kr@chomchob.com>
2022-11-24 18:43:24 +0700

    add : created separate animation file and add it withe variant

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,27 +1,40 @@
 import React from 'react'
 import { motion as m } from 'framer-motion'
+import { container, item } from '../animations'
 
 
 const Contact = () => {
   return (
     <m.main
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1, ease: 'easeOut' }}
+      initial={{ y: '100%' }}
+      animate={{ y: '0%' }}
+      exit={{ opacity: 1 }}
+      transition={{ duration: 0.5, ease: 'easeOut' }}
       className=' bg-red-400 page_style'>
-      <div className='my-96 p-1'>
-        <h1 className='main_title'>Let's Talk</h1>
+      <div className='my-96 lg:my-40 p-1 overflow-hidden'>
+        <m.h1
+          animate={{ y: 0 }}
+          initial={{ y: '100%' }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+          className='main_title'>Let's Talk</m.h1>
       </div>
       <div className="flex gap-40">
         <div className="lg:text-2xl text-base">
           <h4>Find me: </h4>
         </div>
+        
         <div className='lg:text-6xl text-2xl underline '>
-          <ul>
-            <li className='pb-2'>Twitter</li>
-            <li className='pb-2'>Instagram</li>
-            <li className='pb-2'>Facebook</li>
-          </ul>
+          <m.ul variants={container} initial='hidden' animate='show'>
+            <div className='overflow-hidden'>
+              <m.li variants={item} className='pb-2'>Twitter</m.li>
+            </div>
+            <div className='overflow-hidden'>
+              <m.li variants={item} className='pb-2'>Instagram</m.li>
+            </div>
+            <div className='overflow-hidden'>
+              <m.li variants={item} className='pb-2'>Facebook</m.li>
+            </div>
+          </m.ul>
         </div>
       </div>
     </m.main>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import Head from 'next/head'
 import { motion as m } from 'framer-motion'
+import { container, item } from '../animations'
+
 export default function Home() {
   return (
-    <m.div 
-        initial={{opacity: 0}}
-        animate={{opacity: 1}}
-        transition={{duration: 1, ease: 'easeOut'}}
-        className='bg-orange-100 page_style'
+    <m.div
+      initial={{ y: '100%' }}
+      animate={{ y: '0%' }}
+      exit={{ opacity: 1 }}
+      transition={{ duration: 0.5, ease: 'easeOut' }}
+      className='bg-orange-100 page_style'
     >
       <Head>
         <title>Create Next App</title>
@@ -15,15 +18,32 @@ export default function Home() {
       </Head>
 
       <main>
-        <div className='my-96 p-1'>
-          <h1 className='main_title'>Happy Pickle</h1>
+        <div className='my-96 lg:my-40 p-1 overflow-hidden'>
+          <m.h1
+            animate={{ x: 0 }}
+            initial={{ x: '100%' }}
+            transition={{ duration: 0.5, delay: 0.5 }}
+            className='main_title'>Happy Pickle</m.h1>
         </div>
-        <div className='flex justify-between'>
-          <div>
-            <h2>Design</h2>
-            <h2>company</h2>
-            <h2>2022</h2>
-          </div>
+        <div
+          className='flex justify-between '>
+
+          <m.div
+          variants={container}
+          initial='hidden'
+          animate='show'>
+            <div className='overflow-hidden'>
+              <m.h2 variants={item}>Design</m.h2>
+            </div>
+            <div className='overflow-hidden'>
+              <m.h2 variants={item}>company</m.h2>
+            </div>
+            <div className='overflow-hidden'>
+              <m.h2 variants={item}>2022</m.h2>
+            </div>
+          </m.div>
+
+
           <div>
             <h3>This Pickle gonna make you smile.</h3>
             <h3>Scottish designs to make you happy.</h3>
