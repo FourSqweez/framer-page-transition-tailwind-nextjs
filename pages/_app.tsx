147ad1fda4ad61393b65ea25f17206716fbdeba3
Four <jeerasak.kr@chomchob.com>
2022-11-24 16:44:51 +0700

    add : apply font lato to global

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Navbar from '../src/components/Navbar'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className='py-24 px-12 lg:px-48 font-latoBold'>
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
+  )
 }
