21d124fadd8a2f063d45c0d1cab7724d80ae0562
Four <jeerasak.kr@chomchob.com>
2022-11-28 14:36:18 +0700

    change to position fixed and and scroll rout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,13 +3,16 @@ import Link from 'next/link';
 
 const Navbar = () => {
    return (
-      <nav className="text-2xl font-medium z-20 relative">
+      <nav className="text-2xl font-medium w-full py-8 lg:px-48 flex top-0 z-10 fixed">
          <ul className="flex gap-12">
             <Link href={'/'}>
                <li>Home</li>
             </Link>
             <Link href={'/contact'}>
                <li>Contact</li>
+            </Link>
+            <Link href={'/scroll'}>
+               <li>scroll</li>
             </Link>
          </ul>
       </nav>
