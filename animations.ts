170a166068c4a1d05f8547830537a5d796c1b641
Four <jeerasak.kr@chomchob.com>
2022-11-30 14:06:28 +0700

    refactor code

--- animations.ts
+++ animations.ts
@@ -1,58 +1,62 @@
-export const enterPageVariantContainer = {
-   init: {
-      y: '80%',
-   },
-   show: {
-      y: '0%',
-      opacity: 1,
-      transition: { duration: 0.5, ease: 'easeInOut' },
-   },
-   exit: {
-      y: '-100vh',
-      transition: { duration: 0.5, delay: 1 },
-   },
+import { Variants } from 'framer-motion';
+
+export const enterPageVariantContainer: Variants = {
+  init: {
+    y: '80%',
+  },
+  show: {
+    y: '0%',
+    opacity: 1,
+    transition: { duration: 0.5, ease: 'easeInOut' },
+  },
+  exit: {
+    y: '-100vh',
+    transition: { duration: 0.5, delay: 1 },
+  },
 };
 
+export const fadeInTransitionY = {};
+
 export const container = {
-   hidden: { opacity: 0 },
-   show: {
-      opacity: 1,
-      transition: {
-         delayChildren: 0.5,
-         staggerChildren: 0.2,
-      },
-   },
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
 };
 
 export const footerContainer = {
-   hidden: { opacity: 0 },
-   show: {
-      opacity: 1,
-      transition: {
-         delayChildren: 0.5,
-         staggerChildren: 0.1,
-      },
-   },
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.5,
+      staggerChildren: 0.1,
+    },
+  },
 };
 
 export const footerItemContainer = {
-   hidden: { y: '-100%' },
-   show: {
-      y: '0%',
-      transition: {
-         duration: 0.5,
-         delayChildren: 0.5,
-         staggerChildren: 0.2,
-      },
-   },
+  hidden: { y: '-100%' },
+  show: {
+    y: '0%',
+    transition: {
+      duration: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
 };
 
 export const item = {
-   hidden: { x: '-100%' },
-   show: {
-      x: '0%',
-      transition: {
-         duration: 0.5,
-      },
-   },
+  hidden: { x: '-100%' },
+  show: {
+    x: '0%',
+    transition: {
+      duration: 0.5,
+    },
+  },
 };
